d0d9febab6f057d46fb4dc5a8dd3f2f8d0bae36f
arievanwi <arie@avineas.org>
2015-03-14 16:49:30 +0100

    Added pre-transformation option via the system property osgi.jpa.transform

--- osgi.ee.extender.jpa/src/osgi/extender/jpa/service/PersistenceUnitDefinition.java
+++ osgi.ee.extender.jpa/src/osgi/extender/jpa/service/PersistenceUnitDefinition.java
@@ -16,13 +16,18 @@
  */
 package osgi.extender.jpa.service;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -49,24 +54,42 @@ class PersistenceUnitDefinition {
     String validationMode;
     Map<String, String> properties;
 
-    static List<PersistenceUnitDefinition> fromFile(InputStream in) throws Exception {
-        // Get the xsl file.
-        String xsl = "/" + PersistenceUnitDefinition.class.getPackage().getName().replace(".", "/") + "/persistence.xsl";
-        // Open it and use it for transformation.
+    private static Result getTransformer(Result result) throws Exception {
+        SAXTransformerFactory fact = (SAXTransformerFactory) TransformerFactory.newInstance();
+        String xsl = "/" + PersistenceUnitDefinition.class.getPackage().getName().replace(
+                ".", "/") + "/persistence.xsl";
+        TransformerHandler handler;
         try (InputStream xsls = PersistenceUnitDefinition.class.getClassLoader().getResourceAsStream(xsl)) {
-            // Create a new transformer from it.
-            Transformer trans = TransformerFactory.newInstance().newTransformer(new StreamSource(xsls));
-            // Prepare to get the result.
-            StringWriter writer = new StringWriter();
-            StreamResult result = new StreamResult(writer);
-            // Do the transformation.
-            trans.transform(new StreamSource(in), result);
-            // And de-serialize it.
-            XStream stream = new XStream(new DomDriver());
-            @SuppressWarnings("unchecked")
-            List<PersistenceUnitDefinition> list = (List<PersistenceUnitDefinition>) stream.fromXML(writer.getBuffer().toString());
-            return list;
+            // Create the final pipeline.
+            handler = fact.newTransformerHandler(new StreamSource(xsls));
+            handler.setResult(result);
+            String preTransform = System.getProperty("osgi.jpa.transform");
+            if (preTransform != null) {
+                // Construct the transformation pipeline.
+                try (FileInputStream pf = new FileInputStream(preTransform)) {
+                    TransformerHandler preTransformer = fact.newTransformerHandler(new StreamSource(pf));
+                    preTransformer.setResult(new SAXResult(handler));
+                    handler = preTransformer;
+                }
+            }
         } 
+        return new SAXResult(handler);
+    }
+    
+    static List<PersistenceUnitDefinition> fromFile(InputStream in) throws Exception {
+        // Prepare to get the result.
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+        // Construct the non-transforming transformer of the input XML stream.
+        Transformer trans = TransformerFactory.newInstance().newTransformer();
+        // Do the transformation.
+        trans.transform(new StreamSource(in), getTransformer(result));
+        // And de-serialize it.
+        XStream stream = new XStream(new DomDriver());
+        @SuppressWarnings("unchecked")
+        List<PersistenceUnitDefinition> list = 
+            (List<PersistenceUnitDefinition>) stream.fromXML(writer.getBuffer().toString());
+        return list;
     }
 
     @Override
