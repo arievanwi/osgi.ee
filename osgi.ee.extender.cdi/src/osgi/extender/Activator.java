7a4b5b448fe710f97a6724e6f5d0ade9f53a922d
arievanwi <arie@avineas.org>
2015-09-18 09:57:35 +0200

    Bundle stop fix.

--- osgi.ee.extender.cdi/src/osgi/extender/Activator.java
+++ osgi.ee.extender.cdi/src/osgi/extender/Activator.java
@@ -32,24 +32,28 @@ import osgi.extender.resource.impl.ResourceHandlingBundleListener;
 /**
  * Bundle activator. Only takes care of the bundle tracking stuff. All other functionality
  * is handled via SCR components. As such this bundle requires a SCR.
- * 
+ *
  * @author Arie van Wijngaarden
  */
 public class Activator implements BundleActivator {
     private Stream<BundleTracker<Object>> trackers;
-    
+
     @Override
     public void start(BundleContext context) {
-        Collection<BundleTrackerCustomizer<Object>> handlers = 
+        Collection<BundleTrackerCustomizer<Object>> handlers =
                 Arrays.asList(new CdiBundleChangeListener(context.getBundle()),
                         new ResourceHandlingBundleListener());
         trackers = handlers.stream().map((c) ->
-            new BundleTracker<>(context, Bundle.ACTIVE, c));
+        new BundleTracker<>(context, Bundle.ACTIVE, c));
         new Thread(() -> trackers.forEach((t) -> t.open())).start();
     }
 
     @Override
     public void stop(BundleContext context) {
-        trackers.forEach((t) -> t.close());
+        trackers.forEach((t) -> {
+            try {
+                t.close();
+            } catch (Exception e) {}
+        });
     }
 }
