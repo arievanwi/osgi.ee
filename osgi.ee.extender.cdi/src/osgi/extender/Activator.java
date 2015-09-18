13568a4ff68220dc63860237c7867e0d0464eb7b
arievanwi <arie@avineas.org>
2015-09-18 10:01:35 +0200

    Fix in bundle stop.

--- osgi.ee.extender.cdi/src/osgi/extender/Activator.java
+++ osgi.ee.extender.cdi/src/osgi/extender/Activator.java
@@ -18,7 +18,7 @@ package osgi.extender;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
@@ -32,28 +32,24 @@ import osgi.extender.resource.impl.ResourceHandlingBundleListener;
 /**
  * Bundle activator. Only takes care of the bundle tracking stuff. All other functionality
  * is handled via SCR components. As such this bundle requires a SCR.
- *
+ * 
  * @author Arie van Wijngaarden
  */
 public class Activator implements BundleActivator {
-    private Stream<BundleTracker<Object>> trackers;
-
+    private Collection<BundleTracker<Object>> trackers;
+    
     @Override
     public void start(BundleContext context) {
-        Collection<BundleTrackerCustomizer<Object>> handlers =
+        Collection<BundleTrackerCustomizer<Object>> handlers = 
                 Arrays.asList(new CdiBundleChangeListener(context.getBundle()),
                         new ResourceHandlingBundleListener());
         trackers = handlers.stream().map((c) ->
-        new BundleTracker<>(context, Bundle.ACTIVE, c));
+            new BundleTracker<>(context, Bundle.ACTIVE, c)).collect(Collectors.toList());
         new Thread(() -> trackers.forEach((t) -> t.open())).start();
     }
 
     @Override
     public void stop(BundleContext context) {
-        trackers.forEach((t) -> {
-            try {
-                t.close();
-            } catch (Exception e) {}
-        });
+        trackers.forEach((t) -> t.close());
     }
 }
